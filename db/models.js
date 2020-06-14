29f905b761f0603e422b941ab72fb44eaff904d7
salman <salmanvit12345@gmail.com>
2020-06-14 16:24:37 +0530

    first commit

---
+++ db/models.js
@@ -0,0 +1,109 @@
+/**
+ * Created on 14/6/20.
+ */
+const Sequelize = require('sequelize');
+const config = require('../config')
+const dbconfig =config
+
+const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
+    host:config.HOST,
+    dialect:config.DIALECT,
+    pool:{
+        max:5,
+        min:5
+    },
+  });
+
+
+  const FarmerGroup=sequelize.define('farmergroups',{
+    id: {type: Sequelize.DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: Sequelize.DataTypes.STRING,
+    name_of_organization: Sequelize.DataTypes.STRING,
+    institution_type: Sequelize.DataTypes.STRING,
+    district: Sequelize.DataTypes.STRING,
+    block:Sequelize.DataTypes.INTEGER,
+    gram_panchayat:Sequelize.DataTypes.STRING,
+    
+  });
+
+  const Farmer=sequelize.define('farmers',{
+      id:{type: Sequelize.DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+      name: Sequelize.DataTypes.STRING,
+      father_name: Sequelize.DataTypes.STRING,
+      mother_name: Sequelize.DataTypes.STRING,
+      spouse_name: Sequelize.DataTypes.STRING,
+      sex:Sequelize.DataTypes.STRING,
+      phone_number:Sequelize.DataTypes.STRING,
+      designation:Sequelize.DataTypes.STRING,
+      category:Sequelize.DataTypes.STRING,
+      land_in_holding:Sequelize.DataTypes.STRING
+  });
+
+  const FC=sequelize.define('fcdata',{
+    id:{type: Sequelize.DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: Sequelize.DataTypes.STRING, 
+    phone_number:Sequelize.DataTypes.STRING,
+    sex:Sequelize.DataTypes.STRING,
+  });
+
+  const CRP=sequelize.define('crpdata',{
+    id:{type: Sequelize.DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: Sequelize.DataTypes.STRING, 
+    phone_number:Sequelize.DataTypes.STRING,
+    sex:Sequelize.DataTypes.STRING,
+  });
+
+  const MKS=sequelize.define('mksdata',{
+    id:{type: Sequelize.DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: Sequelize.DataTypes.STRING, 
+    phone_number:Sequelize.DataTypes.STRING,
+    sex:Sequelize.DataTypes.STRING,
+  });
+
+  const Meeting=sequelize.define('meetings',{
+      id:{type: Sequelize.DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+      description:Sequelize.DataTypes.STRING,
+      location:Sequelize.DataTypes.STRING,
+      schdeuled_on:Sequelize.DataTypes.DATE,
+      remarks:Sequelize.DataTypes.STRING
+  });
+
+  const Survey=sequelize.define('surveys',{
+    id:{type: Sequelize.DataTypes.INTEGER, primaryKey: true, autoIncrement: true}, 
+    description:Sequelize.DataTypes.STRING,  
+  })
+
+  const SurveyQuestion=sequelize.define('surveyquestions',{
+    id:{type: Sequelize.DataTypes.INTEGER, primaryKey: true, autoIncrement: true}, 
+    question:Sequelize.DataTypes.STRING,  
+  })
+
+  const SurveyQuesAnswerOptions=sequelize.define('surveyquestionansweroptions',{
+      id:{type: Sequelize.DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+      answer:Sequelize.DataTypes.STRING
+  })
+
+  // Adding Associations for the tables
+
+  FarmerGroup.hasMany(Farmer);
+  //Farmer.belongsTo(FarmerGroup);
+
+  CRP.hasMany(Farmer);
+  //Farmer.belongsTo(CRP);
+
+  FC.hasMany(CRP);
+ // CRP.belongsTo(FC);
+
+  MKS.hasMany(FC);
+  //FC.belongsTo(MKS);
+
+  CRP.hasMany(Meeting);
+ // Meeting.belongsTo(CRP);
+
+  Meeting.belongsToMany(Farmer,{through:'farmerMeeting'});
+
+
+  exports=module.exports={
+    FarmerGroup,Farmer,FC,CRP,MKS,Meeting,DATABASE: sequelize
+}
+
---
+++ index.js
@@ -0,0 +1,29 @@
+/**
+ * Created on 14/6/20.
+ */
+
+const express=require('express')
+const path=require('path')
+const app=express()
+const SERVER_PORT=2020
+const sequelize=require('./db/models').DATABASE
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use('/', express.static(path.join(__dirname, 'public')))
+//app.use('/api/v1',require('./routes/api_v1'))
+// app.listen(SERVER_PORT,()=>{
+//     console.log("servr started")
+// })
+
+sequelize.sync({force:true}).then(function () {
+    console.log("Database Configured");
+    
+  })
+  .catch((err)=>{
+    console.log(err)
+    })
+
+    app.listen(SERVER_PORT,()=>{
+        console.log("started the base")
+    })
